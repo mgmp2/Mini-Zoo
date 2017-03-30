78952882a2f71cd5cceb8caf782e73657f6b566d
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-30 16:17:58 -0500

    change color of img

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+
+  var img = document.getElementsByClassName('animal');
+
+
+  select.onchange = function(){
+    if(select.value =="original") {
+      for (var i = 0; i < img.length; i++) {
+        img[i].classList.add("original");
+        img[i].classList.remove("blanco-negro");
+        img[i].classList.remove("invertir-colores");
+        img[i].classList.remove("sepia");
+      }
+    } if(select.value =="blanco-negro"){
+      for (var i = 0; i < img.length; i++) {
+        img[i].classList.add("blanco-negro");
+        img[i].classList.remove("original");
+        img[i].classList.remove("invertir-colores");
+        img[i].classList.remove("sepia");
+
+      }
+    } if(select.value =="invertir-colores") {
+        for (var i = 0; i < img.length; i++) {
+          img[i].classList.add("invertir-colores");
+          img[i].classList.remove("original");
+          img[i].classList.remove("blanco-negro");
+          img[i].classList.remove("sepia");
+        }
+    } if (select.value =="sepia") {
+          for (var i = 0; i < img.length; i++) {
+            img[i].classList.add("sepia");
+            img[i].classList.remove("original");
+            img[i].classList.remove("blanco-negro");
+            img[i].classList.remove("invertir-colores");
+          }
+  }
+
+};
