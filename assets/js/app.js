4fcaaf6305b997804a539c1d6399db4c657025e4
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-30 18:04:34 -0500

    remove little code

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@
     } if(select.value =="blanco-negro"){
       for (var i = 0; i < img.length; i++) {
         img[i].classList.add("blanco-negro");
-        img[i].classList.remove("original");
         img[i].classList.remove("invertir-colores");
         img[i].classList.remove("sepia");
 
@@ -20,14 +19,12 @@
     } if(select.value =="invertir-colores") {
         for (var i = 0; i < img.length; i++) {
           img[i].classList.add("invertir-colores");
-          img[i].classList.remove("original");
           img[i].classList.remove("blanco-negro");
           img[i].classList.remove("sepia");
         }
     } if (select.value =="sepia") {
           for (var i = 0; i < img.length; i++) {
             img[i].classList.add("sepia");
-            img[i].classList.remove("original");
             img[i].classList.remove("blanco-negro");
             img[i].classList.remove("invertir-colores");
           }
